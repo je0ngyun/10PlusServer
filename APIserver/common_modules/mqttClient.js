a12953d670a26df013232bd586ba36b227d36fec
김정윤 <kjyjh9930@gmail.com>
2021-06-05 23:56:27 +0900

    refactor : 불필요 주석 제거

--- APIserver/common_modules/mqttClient.js
+++ APIserver/common_modules/mqttClient.js
@@ -21,23 +21,6 @@ self.start = function () {
       let info = JSON.parse(message);
       await db.setDevice(info);
     }
-    // } else if (topic === stateTopic) {
-    //   //상태처리 토픽
-    //   let info = JSON.parse(message);
-    //   let computed = { host: '', name: '', state: '' };
-    //   computed.host = info.device_host;
-    //   computed.name = await db.getDeviceName({ host: info.device_host + '' });
-    //   //DB에 문자열로 저장하기 위해 2진수를 평문으로 변환
-    //   for (let i = 0; i < info.device_state.length; i++) {
-    //     if (Number(info.device_state[i]) === 0) {
-    //       computed.state += 'false,';
-    //     } else {
-    //       computed.state += 'true,';
-    //     }
-    //   }
-    //   computed.state = computed.state.slice(0, computed.state.length - 1);
-    //   await db.setDeviceLog(computed);
-    // }
   });
 };
 
