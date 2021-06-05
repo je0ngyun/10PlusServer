56b9079cdcf37eafcbf34ac10009c9f651201a4d
김정윤 <kjyjh9930@gmail.com>
2021-06-05 11:24:24 +0900

    feat : express 내부에서 aedes broker 구축 및 테스트 클라이언트 추가

---
+++ APIserver/common_modules/mqttBroker.js
@@ -0,0 +1,16 @@
+'use strict';
+//MQTT 브로커
+const env = require('./env/env.json');
+const db = require('./dbmodel');
+const aedes = require('aedes')();
+const server = require('net').createServer(aedes.handle);
+const port = 1883;
+const self = {};
+
+self.start = function () {
+  server.listen(port, function () {
+    console.log(`MQTT Broker Start : ${port}`);
+  });
+};
+
+module.exports = self;
